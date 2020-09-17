b316d71abf1b8b1e7a4f9144b07c76a972428697
Mike Chester <mikeches@amazon.com>
2020-09-17 11:54:44 -0700

    latest

--- src/App.js
+++ src/App.js
@@ -1,32 +1,72 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './aws_logo.png';
 import './App.css';
-import { withAuthenticator } from 'aws-amplify-react'
-import Amplify, { Auth } from 'aws-amplify';
+import { Authenticator, SignIn } from 'aws-amplify-react'
+import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
-class App extends Component {
-  render() {
+class CustomSignIn extends SignIn {
+  showComponent() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <img src={logo} alt="logo" style={{height: "50px", width: "84px", paddingBottom: "30px"}}/>
+        <div className="aws-signin-general-label-box">
+          <div className="aws-signin-general-link-box-no-margin main-table">
+            <div className="aws-signin-float-left" style={{width: "40%"}}><b>Email</b></div>
+          </div>
+        </div>
+        <div className="aws-signin-general-input-box">
+          <input onChange={this.handleInputChange} id="username" key="username" name="username" className="aws-signin-textfield" autocorrect="off" autocapitalize="off" />
+          <div id="password_input_alert" className="signin-input-warning" style={{display: "none"}}>
+            <span id="password_input_alert_content" className="awsui-text-small aws-signin-color-red"></span>
+          </div>
+        </div>
+        <div className="aws-signin-general-label-box">
+            <div className="aws-signin-general-link-box-no-margin main-table">
+                <div className="aws-signin-float-left" style={{width: "40%"}}><b>Password</b></div>
+                <div className="aws-signin-float-right" style={{textAlign: "right; width:58%;"}}>
+                    <span id="root_forgot_password_link">Forgot password?</span>
+                </div>
+            </div>
+        </div>
+        <div className="aws-signin-general-input-box">
+            <input onChange={this.handleInputChange} id="password" key="password" name="password" type="password" className="aws-signin-textfield" autocorrect="off" autocapitalize="off" />
+            <div id="password_input_alert" className="signin-input-warning" style={{display: "none"}}>
+                <span id="password_input_alert_content" className="awsui-text-small aws-signin-color-red"></span>
+            </div>
+        </div>
+        <div className="aws-signin-general-button-box">
+            <button onClick={() => super.signIn()} id="signin_button" type="button" className="aws-signin-button aws-signin-button-size-normal aws-signin-button-variant-primary" style={{width: "100%"}}><span id="signin_button_text" style={{color: "white; font-weight: bold;"}}>Sign in</span></button>
+        </div>
       </div>
     );
   }
 }
 
-export default withAuthenticator(App, true);
+class App extends React.Component {
+  render() {
+    if (this.props.authState === "signedIn") {
+      return (
+        <div>
+          <h1>Internal App</h1>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
+}
+
+class Root extends Component {
+  render() {
+    return (
+      <Authenticator hide={[SignIn]} amplifyConfig={aws_exports}>
+        <CustomSignIn />
+        <App />
+      </Authenticator>
+    );
+  }
+}
+
+export default Root;
